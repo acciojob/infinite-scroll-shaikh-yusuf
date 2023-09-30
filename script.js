dbdf793bc8c7c84f60e6f40dfab6f9c3237e6773
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-09-30 10:54:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
 
+const scrollContainer=document.getElementById('infi-list');
+let i=0;
+function addOneitem() {
+	const li=document.creatElement('li');
+	li.innerText=`Item ${++i}`;
+	scrollContainer.appendChild(li);
+	
+}
+
+for(let i=0;i<10;i++)
+	{
+		addOneitem()
+	}
+scrollContainer.addEventListener('scroll',()=>{
+	const maxScrollHight=
+		scrollContainer.scrollHeight-scrollContainer.clientHeight;
+	let currentScrolltop=scrollContainer.scrollTop;
+	let thresold=Math.abs(maxScrollHight-currentScrolltop)
+	if(th<=5){
+		for(let i=0;i<2;i++)
+			{
+				addOneitem()
+			}
+	}
+})
