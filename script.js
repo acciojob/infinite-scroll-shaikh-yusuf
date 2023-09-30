58d403f91c296269b9fe344cf700b32badc335c0
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-09-30 10:57:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-//your code here!
-
-const scrollContainer=document.getElementById('infi-list');
-let i=0;
-function addOneitem() {
-	const li=document.creatElement('li');
-	li.innerText=`Item ${++i}`;
-	scrollContainer.appendChild(li);
-	
+const scrollContainer = document.getElementById("infi-list");
+let i = 0;
+function addOneItem() {
+  const li = document.createElement("li");
+  li.innerText = `Item ${++i}`;
+  scrollContainer.appendChild(li);
 }
 
-for(let i=0;i<10;i++)
-	{
-		addOneitem()
-	}
-scrollContainer.addEventListener('scroll',()=>{
-	const maxScrollHight=
-		scrollContainer.scrollHeight-scrollContainer.clientHeight;
-	let currentScrolltop=scrollContainer.scrollTop;
-	let thresold=Math.abs(maxScrollHight-currentScrolltop)
-	if(th<=5){
-		for(let i=0;i<2;i++)
-			{
-				addOneitem()
-			}
-	}
-})
+for (let i = 0; i < 10; i++) {
+  addOneItem();
+}
+
+scrollContainer.addEventListener("scroll", () => {
+  const maxScrollHeight =
+    scrollContainer.scrollHeight - scrollContainer.clientHeight;
+  let currentScrollTop = scrollContainer.scrollTop;
+
+  let threshold = Math.abs(maxScrollHeight - currentScrollTop);
+
+  if (threshold <= 5) {
+    for (let i = 0; i < 2; i++) {
+      addOneItem();
+    }
+  }
+});
